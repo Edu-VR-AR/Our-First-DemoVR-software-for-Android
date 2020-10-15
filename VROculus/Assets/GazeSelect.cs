6f3f2d7b0e37fcdd978db33a72f8771aca0e3ed9
EgorKarpov <argon32a@gmail.com>
2020-10-15 13:27:01 +0600

    First version V_001

---
+++ VROculus/Assets/GazeSelect.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GazeSelect : MonoBehaviour
+{
+
+    float speed = 30.0f;
+    bool spin = false;
+    bool bScale = false;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float sc = bScale ? 4.0f : 3.0f;
+
+        transform.localScale = new Vector3(sc, sc, sc);
+
+        if (spin)
+        {
+            transform.Rotate(Vector3.up, speed * Time.deltaTime);
+        }
+
+       
+
+        }
+
+    public void GazeFocusStart()
+    {
+        spin = true;
+    }
+
+    public void GazeFocusEnd()
+    {
+        spin = false;
+    }
+
+    public void GazeFocusClick()
+    {
+        bScale = !bScale;
+    }
+}
